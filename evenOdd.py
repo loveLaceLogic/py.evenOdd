e88c938d69877a76139491837599a32308e6ac76
Kristina C. <kristinachaleunsak@gmail.com>
2025-03-16 15:54:00 -0500

    Add files via upload

---
+++ evenOdd.py
@@ -0,0 +1,7 @@
+#Determining whether a number is even or odd
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
