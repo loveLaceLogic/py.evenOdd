4b41e0898b5563b85092d5192b8d069bc37415c5
Kristina C. <kristinachaleunsak@gmail.com>
2025-03-16 17:23:48 -0500

    Update evenOdd.py

--- evenOdd.py
+++ evenOdd.py
@@ -1,7 +1,10 @@
-#Determining whether a number is even or odd
+# Program to determine whether a number is even or odd
+
+# Get user input for a number
 number = int(input("Enter a number: "))
 
+# Check if the number is divisible by 2
 if number % 2 == 0:
-    print("The number is even.")
+    print("The number is even.")  # Output if the number is even
 else:
-    print("The number is odd.")
+    print("The number is odd.")   # Output if the number is odd
